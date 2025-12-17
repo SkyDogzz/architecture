e0a019306272286a5651a59f54c0e39692be9fa2
Thomas Stephan <tstephan@f2r3s15.clusters.42paris.fr>
2025-12-17 17:25:14 +0100

    ?

--- core/include/Core/Platform/Window.hpp
+++ core/include/Core/Platform/Window.hpp
@@ -1,35 +1,37 @@
 
 #pragma once
-#include "Core/Runtime/Events/Event.hpp"
 #include <functional>
+#include <memory>
 #include <string>
+
+#include "Core/Runtime/Events/Event.hpp"
 
 namespace Core {
 
 struct WindowProps {
-  std::string Title = "Core Window";
-  int Width = 1280;
-  int Height = 720;
-  bool VSync = true;
+	std::string Title = "Core Window";
+	int			Width = 1280;
+	int			Height = 720;
+	bool		VSync = true;
 };
 
 class Window {
 public:
-  using EventCallbackFn = std::function<void(Event &)>;
+	using EventCallbackFn = std::function<void(Event&)>;
 
-  virtual ~Window() = default;
+	virtual ~Window() = default;
 
-  virtual void OnUpdate() = 0;
-  virtual int GetWidth() const = 0;
-  virtual int GetHeight() const = 0;
+	virtual void OnUpdate() = 0;
+	virtual int	 GetWidth() const = 0;
+	virtual int	 GetHeight() const = 0;
 
-  virtual void SetEventCallback(const EventCallbackFn &cb) = 0;
-  virtual void SetVSync(bool enabled) = 0;
-  virtual bool IsVSync() const = 0;
+	virtual void SetEventCallback(const EventCallbackFn& cb) = 0;
+	virtual void SetVSync(bool enabled) = 0;
+	virtual bool IsVSync() const = 0;
 
-  virtual void *GetNativeHandle() const = 0;
+	virtual void* GetNativeHandle() const = 0;
 
-  static std::unique_ptr<Window> Create(const WindowProps &props);
+	static std::unique_ptr<Window> Create(const WindowProps& props);
 };
 
-} // namespace Core
+}  // namespace Core
