15a0bed88d00aa775b59063dc24219167d579333
Thomas Stephan <tstephan@f2r3s15.clusters.42paris.fr>
2025-12-17 17:02:00 +0100

    rm cache

--- core/src/Core/Graphics/OpenGL/OpenGLContext.cpp
+++ core/src/Core/Graphics/OpenGL/OpenGLContext.cpp
@@ -1,42 +1,38 @@
 
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
 #include "Core/Foundation/Assert.hpp"
 #include "Core/Foundation/Log.hpp"
 #include "Core/Graphics/GraphicsContext.hpp"
-
-#include <GLFW/glfw3.h>
-#include <glad/glad.h>
 
 namespace Core {
 
 class OpenGLContext final : public GraphicsContext {
 public:
-  explicit OpenGLContext(GLFWwindow *window) : m_Window(window) {
-    CORE_ASSERT(window, "OpenGLContext: window is null");
-  }
+	explicit OpenGLContext(GLFWwindow* window) : m_Window(window) {
+		CORE_ASSERT(window, "OpenGLContext: window is null");
+	}
 
-  void Init() override {
-    glfwMakeContextCurrent(m_Window);
+	void Init() override {
+		glfwMakeContextCurrent(m_Window);
 
-    CORE_ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress),
-                "Failed to initialize GLAD");
+		CORE_ASSERT(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress), "Failed to initialize GLAD");
 
-    Log(LogLevel::Info,
-        std::string("OpenGL Vendor: ") + (const char *)glGetString(GL_VENDOR));
-    Log(LogLevel::Info, std::string("OpenGL Renderer: ") +
-                            (const char *)glGetString(GL_RENDERER));
-    Log(LogLevel::Info, std::string("OpenGL Version: ") +
-                            (const char *)glGetString(GL_VERSION));
+		Log(LogLevel::Info, std::string("OpenGL Vendor: ") + (const char*)glGetString(GL_VENDOR));
+		Log(LogLevel::Info, std::string("OpenGL Renderer: ") + (const char*)glGetString(GL_RENDERER));
+		Log(LogLevel::Info, std::string("OpenGL Version: ") + (const char*)glGetString(GL_VERSION));
 
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  }
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
 
 private:
-  GLFWwindow *m_Window;
+	GLFWwindow* m_Window;
 };
 
-std::unique_ptr<GraphicsContext> GraphicsContext::Create(GLFWwindow *window) {
-  return std::make_unique<OpenGLContext>(window);
+std::unique_ptr<GraphicsContext> GraphicsContext::Create(GLFWwindow* window) {
+	return std::make_unique<OpenGLContext>(window);
 }
 
-} // namespace Core
+}  // namespace Core
